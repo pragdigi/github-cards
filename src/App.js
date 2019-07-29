284cc75661b28641725c95a9a4d55dde31ddb663
Krste <krste@kryptomine.com.au>
2019-07-29 14:25:16 +0200

    add CardList component and revise Card component to test with test data.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from "react";
 import "./App.css";
-import placeholder from "./75.png"
 
-/*
 	const testData = [
 			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
       {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
   		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
 	];
-*/
+
+
+const CardList = (props) => (
+<div>
+  <Card />
+</div>
+);
+
 class Card extends React.Component {
   render() {
+    const profile = testData[0];
     return (
-      <div className="github-profile" /* style={{ height: 10 }} */>
-        <img src={placeholder} alt={"placeholder"}/>
+      <div className="github-profile">
+        <img src={profile.avatar_url} alt={"placeholder"}/>
         <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     );
@@ -28,7 +34,7 @@ class App extends React.Component {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Card />
+        <CardList />
       </div>
     );
   }

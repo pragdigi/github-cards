9db0f68b9f3847f1bcc81f6d48b2c15ad21327e1
Krste <krste@kryptomine.com.au>
2019-07-29 17:21:32 +0200

    complete imput form using state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React from "react";
 import "./App.css";
 
-	const testData = [
-			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-	];
+const testData = [
+  {
+    name: "Dan Abramov",
+    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+    company: "@facebook"
+  },
+  {
+    name: "Sophie Alpert",
+    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+    company: "Humu"
+  },
+  {
+    name: "Sebastian Markbåge",
+    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+    company: "Facebook"
+  }
+];
 
-
-const CardList = (props) => (
-<div>
-  {testData.map(profile => <Card {...profile} />)}
-</div>
+const CardList = props => (
+  <div>
+    {props.profiles.map(profile => (
+      <Card {...profile} />
+    ))}
+  </div>
 );
+
+class Form extends React.Component {
+  state = { userName : '' };
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(
+      this.state.userName
+    )
+  };
+  render() {
+    return (
+      <form action="" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
+          placeholder="GitHub username"
+          required
+        />
+        <button>Add Card</button>
+      </form>
+    );
+  }
+}
 
 class Card extends React.Component {
   render() {
     const profile = this.props;
     return (
       <div className="github-profile">
-        <img src={profile.avatar_url} alt={"placeholder"}/>
+        <img src={profile.avatar_url} alt={"placeholder"} />
         <div className="info">
           <div className="name">{profile.name}</div>
           <div className="company">{profile.company}</div>
@@ -30,11 +67,16 @@ class Card extends React.Component {
 }
 
 class App extends React.Component {
+  state = {
+    profiles: testData
+  };
+
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <CardList />
+        <Form />
+        <CardList profiles={this.state.profiles} />
       </div>
     );
   }

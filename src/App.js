6ac9d47fad5287e98730085c419418225233d370
Krste <krste@kryptomine.com.au>
2019-07-29 14:19:24 +0200

    fix style issue, image and add test data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./App.css";
 import placeholder from "./75.png"
 
+/*
+	const testData = [
+			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+	];
+*/
 class Card extends React.Component {
   render() {
     return (
-      <div className="github-profile">
+      <div className="github-profile" /* style={{ height: 10 }} */>
         <img src={placeholder} alt={"placeholder"}/>
         <div className="info">
           <div className="name">Name here...</div>

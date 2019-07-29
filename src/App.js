484c2fd7912f598a19d44e3de0ac6766c0e70243
Krste <krste@kryptomine.com.au>
2019-07-29 11:55:14 +0200

    changed Card and App to class and modified styling. css contains issue, troubleshooting.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './App.css';
 
-const App = ({title}) => (
-  <div className="header">{title}</div>
-);
+class Card extends React.Component {
+	render() {
+  	return (
+    	<div className="github-profile">
+    	  <img src="75"/>
+        <div className="info">
+          <div className="name">Name here...</div>
+          <div className="company">Company here...</div>
+        </div>
+    	</div>
+    );
+  }
+}
+
+class App extends React.Component {
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Card />
+    	</div>
+    );
+  }	
+}
 
 export default App;

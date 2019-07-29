32d2d4c75990f0ab53c57a2caa51efed72a48c98
Krste <krste@kryptomine.com.au>
2019-07-29 13:58:02 +0200

    fixed image, used import instead of directly referecing.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import placeholder from "./75.png"
 
 class Card extends React.Component {
-	render() {
-  	return (
-    	<div className="github-profile">
-    	  <img src="75"/>
+  render() {
+    return (
+      <div className="github-profile">
+        <img src={placeholder} alt={"placeholder"}/>
         <div className="info">
           <div className="name">Name here...</div>
           <div className="company">Company here...</div>
         </div>
-    	</div>
+      </div>
     );
   }
 }
 
 class App extends React.Component {
-	render() {
-  	return (
-    	<div>
-    	  <div className="header">{this.props.title}</div>
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
         <Card />
-    	</div>
+      </div>
     );
-  }	
+  }
 }
 
 export default App;

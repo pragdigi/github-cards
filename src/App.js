b8413999df2e273bf2d5686cce497cc157991920
Krste <krste@kryptomine.com.au>
2019-07-29 11:35:40 +0200

    fixed issue with running app (mountNode). now displays app.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import './App.css';
-import ReactDOM from 'react-dom';
 
 const App = ({title}) => (
   <div className="header">{title}</div>
-);
-
-ReactDOM.render(
-	<App title="The GitHub Cards App" />,
-  mountNode,
 );
 
 export default App;
